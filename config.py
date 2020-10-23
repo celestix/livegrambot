f2e657c894b74e6bfd1db6890456e6202b8597a2
khumoyun2008 <63740404+khumoyun2008@users.noreply.github.com>
2020-10-23 15:01:11 +0500

    Add files via upload
    Files are UPLOADED

---
+++ config.py
@@ -0,0 +1,5 @@
+start = "Hello! This is your start message!"
+ban = "you were banned by the admin!"
+unban = "you were unbanned by the admin."
+text_message = "Message that would be send if somebody writes any text"
+main_id = 927266710 #id without quotes
