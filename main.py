e6179086af704e2999e7efb26770a9f7d47980f5
khumoyun2008 <63740404+khumoyun2008@users.noreply.github.com>
2020-10-30 15:45:38 +0500

    Add files via upload
    New version!
    In this version you can send location and ban/unban users

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ sql.execute('''CREATE TABLE IF NOT EXISTS USERS(
     first_name VARCHAR,
     messageid INT,
     message VARCHAR)''')
+sql.execute('''CREATE TABLE IF NOT EXISTS blocked(
+    user_id INT)''')
     
 import config
-
-bot = telebot.TeleBot("1297441208:AAHiknb7UcAXgHIFTcwpjw5Tj5iplqI3d0Q")
+bot = telebot.TeleBot(config.TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -19,29 +20,75 @@ def start(message):
         bot.send_message(message.chat.id, config.start)
     except Exception as e:
         print(str(e))
+@bot.message_handler(commands=["ban"])
+def blocked(message):
+    try:
+        if message.from_user.id == config.main_id:
+            #fromm = str(message.from_user.id)
+            #name = message.from_user.first_name
+            sql.execute("SELECT user_id FROM USERS WHERE messageid = ?",(message.reply_to_message.message_id,))
+            db.commit()
+            Lusers = sql.fetchall()
+            for i in Lusers:
+                print(i[0])
+                sql.execute("SELECT user_id FROM blocked WHERE user_id = ?",(i[0],))
+                if sql.fetchone() is None:
+                    bot.send_message(i[0],config.ban)
+                    bot.send_message(message.chat.id, "you blocked " + str(i[0]))
+                    sql.execute("INSERT INTO blocked VALUES (?)",(i[0],))
+                    db.commit()
+        else:
+            bot.send_message(message.chat.id, "you are not admin!")
+    except Exception as ee:
+        print("error in block" + str(ee))
+@bot.message_handler(commands=["unban"])
+def unblocked(message):
+    try:
+        if message.from_user.id == config.main_id:
+            sql.execute("SELECT user_id FROM USERS WHERE messageid = ?",(message.reply_to_message.message_id,))
+            db.commit()
+            Lusers = sql.fetchall()
+            for i in Lusers:
+                print(str(i[0]) + " mine")
+                sql.execute("SELECT user_id FROM blocked WHERE user_id = ?",(i[0],))
+                db.commit()
+                print("unbanning")
+                sql.execute("DELETE FROM blocked WHERE user_id = ?",(i[0],))
+                db.commit()
+                bot.send_message(i[0],"you were unblocked")
+                bot.send_message(message.chat.id, "you unblocked " + str(i[0]))
+        else:
+            bot.send_message(message.chat.id, "you are not admin!")
+    except Exception as ee:
+        print("error in block" + str(ee))
 @bot.message_handler(content_types=['text'])
 def text(message):
     try:
-        if message.from_user.id != config.main_id:
-            q = bot.forward_message(config.main_id, message.chat.id, message.message_id)
-            sql.execute("INSERT OR IGNORE INTO USERS VALUES(?,?,?,?)",(message.from_user.id,message.from_user.first_name, q.message_id, message.text))
-            db.commit()
-            bot.send_message(message.chat.id, config.text_message)
-            print(message.message_id)
-        elif message.from_user.id == config.main_id:
-            if message.reply_to_message is None:
-                bot.forward_message(config.main_id, message.chat.id, message.message_id)
-                sql.execute("INSERT INTO USERS VALUES(?,?,?,?)",(message.from_user.id,message.from_user.first_name, message.message_id, message.text))
+        sql.execute("SELECT user_id FROM blocked WHERE user_id = ?",(message.from_user.id,))
+        db.commit()
+        if sql.fetchone() is not None:
+            bot.send_message(message.chat.id, config.banned)
+        else:
+            if message.from_user.id != config.main_id:
+                q = bot.forward_message(config.main_id, message.chat.id, message.message_id)
+                sql.execute("INSERT OR IGNORE INTO USERS VALUES(?,?,?,?)",(message.from_user.id,message.from_user.first_name, q.message_id, message.text))
                 db.commit()
                 bot.send_message(message.chat.id, config.text_message)
-            elif message.reply_to_message is not None:
-                print(message.reply_to_message.message_id)
-                sql.execute("SELECT user_id FROM USERS WHERE messageid = ?",(message.reply_to_message.message_id,))
-                db.commit()
-                Lusers = sql.fetchall()
-                for i in Lusers:
-                    print(i[0])
-                    bot.send_message(i[0], message.text)
+                print(message.message_id)
+            elif message.from_user.id == config.main_id:
+                if message.reply_to_message is None:
+                    bot.forward_message(config.main_id, message.chat.id, message.message_id)
+                    sql.execute("INSERT INTO USERS VALUES(?,?,?,?)",(message.from_user.id,message.from_user.first_name, message.message_id, message.text))
+                    db.commit()
+                    bot.send_message(message.chat.id, config.text_message)
+                elif message.reply_to_message is not None:
+                    print(message.reply_to_message.message_id)
+                    sql.execute("SELECT user_id FROM USERS WHERE messageid = ?",(message.reply_to_message.message_id,))
+                    db.commit()
+                    Lusers = sql.fetchall()
+                    for i in Lusers:
+                        print(i[0])
+                        bot.send_message(i[0], message.text)
     except Exception as e:
         print(str(e))        
                 
@@ -50,11 +97,17 @@ def text(message):
 def other(message):
     try:
         if message.from_user.id != config.main_id:
-            q = bot.forward_message(config.main_id, message.chat.id, message.message_id)
-            sql.execute("INSERT OR IGNORE INTO USERS VALUES(?,?,?,?)",(message.from_user.id,message.from_user.first_name, q.message_id, message.text))
+            sql.execute("SELECT user_id FROM blocked WHERE user_id = ?",(message.from_user.id,))
             db.commit()
-            bot.send_message(message.chat.id, config.text_message)
-            print(message.message_id)
+            b = sql.fetchall()
+            if sql.fetchone is not None:
+                bot.send_message(message.chat.id, config.banned)
+            else:
+                q = bot.forward_message(config.main_id, message.chat.id, message.message_id)
+                sql.execute("INSERT OR IGNORE INTO USERS VALUES(?,?,?,?)",(message.from_user.id,message.from_user.first_name, q.message_id, message.text))
+                db.commit()
+                bot.send_message(message.chat.id, config.text_message)
+                print(message.message_id)
         elif message.from_user.id == config.main_id:
             if message.reply_to_message is None:
                 bot.forward_message(config.main_id, message.chat.id, message.message_id)
@@ -81,7 +134,7 @@ def other(message):
                     elif message.content_type == "document":
                         bot.send_document(i[0], message.document.file_id)
                     elif message.content_type == "location":
-                        bot.send_location(i[0], message.location)
+                        bot.send_location(i[0], message.location.longitude, message.location.latitude)
                     elif message.content_type == "animation":
                         bot.send_animation(i[0], message.animation.file_id)
                     elif message.content_type == "contact":
