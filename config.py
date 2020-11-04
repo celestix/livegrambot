2335c8912659f8a8f7ebafe14b26202a279bcf93
khumoyun2008 <63740404+khumoyun2008@users.noreply.github.com>
2020-11-04 15:07:56 +0500

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-
-
-start = "Hello! This is your start message!"
-ban = "you were banned by the admin!"
-unban = "you were unbanned by the admin."
-text_message = "Message that would be send if somebody writes any text"
-banned = "you are blocked"
-main_id = telegramid #id without quotes
-TOKEN = "TOKEN"
